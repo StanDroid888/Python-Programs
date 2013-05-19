eea457bffa546f727c32fe7b47582cf12d725d6d
StanDroid888 <StanDroid888@gmail.com>
2013-05-19 13:57:57 -0400

    Updated WordClass.py with @property

--- WordClass.py
+++ WordClass.py
@@ -1,12 +1,36 @@
 #!/usr/bin/python
+# WordClass.py by Stanley Wong
 class Word:
 	
-	wordCount = 0
-	linesWhichAppears = []
-	theWord = ""
+	_wordCount = 0
+	_linesWhichAppears = []
+	_theWord = ""
 
 	def __init__(self, wordString, lineNumber):
 		self.theWord = wordString
             	self.wordCount = 1
 	    	self.linesWhichAppears = [lineNumber]
-	
+
+	@property
+	def wordCount(self):
+		return self._wordCount
+
+	@wordCount.setter
+	def wordCount(self,wordCount):
+		self._wordCount=wordCount
+
+	@property
+	def linesWhichAppears(self):
+		return self._linesWhichAppears
+
+	@linesWhichAppears.setter
+	def linesWhichAppears(self,linesWhichAppears):
+		self._linesWhichAppears = linesWhichAppears
+
+	@property
+	def theWord(self):
+		return self._theWord
+
+	@theWord.setter
+	def theWord(self, theWord):
+		self._theWord = theWord
